7d9b7959f7f6261e0edc6276198224373d11e982
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2022-06-12 12:16:16 -0700

    Basis for iterating over the file
    Most of this will probably get thrown away but figured I'll just keep
    it here for now

--- Makefile
+++ Makefile
@@ -1,15 +1,25 @@
-CC=gcc
+CC=clang
 # These are mostly here to on to cover our buts during the base setup
-DEVFLAGS=-Werror -Wall -Wextra -fsanitize=address -fsanitize=undefined
+DEVFLAGS= -Wall -Wextra -fsanitize=address -fsanitize=undefined # -Werror
+CLANGFLAGS=-MJ tmp.json
 CFLAGS=$(DEVFLAGS)
 
 all: rgb2ycc
+
+compile_commands.json: tmp.json
+	echo '[' > $@
+	cat $< >> $@
+	echo ']' >> $@
+
+tmp.json: main.c
+	$(CC) $(CLANGFLAGS) $(CFLAGS) $< -o $@
+
 
 rgb2ycc: main.c
 	$(CC) $(CFLAGS) $< -o $@
 
 test: rgb2ycc
-	$< sample_640×426.bmp > output_sample_640×426.bmp
+	./$< sample_640×426.bmp # > output_sample_640×426.bmp
 
 clean:
 	$(RM) output_sample_640×426.bmp
--- main.c
+++ main.c
@@ -1,7 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#define BUFFERSIZE 41
 
+#define DEBUG
+#ifdef DEBUG
+# define DEBUG_PRINT(x) printf x
+#else
+# define DEBUG_PRINT(x) do {} while (0)
+#endif
+
+#define dbp()
 int main(int argc, char* argv[])
 {
 
@@ -24,5 +33,45 @@ int main(int argc, char* argv[])
     exit(2);
   }
 
-  printf("hello world");
+  /* FILE IS NOW OPEN */
+  /* char buffer[BUFFERSIZE]; /\* buffer for storing the string from the file  *\/ */
+  char place_holder; /* char used to hold the current character for checking its value */
+  int length = BUFFERSIZE;
+
+  int i = 0;
+  /* place_holder = fgetc(file_pointer); */
+  /* header */
+  for (int j = 0; i-j < 2; i ++ ){
+    DEBUG_PRINT(("place_holder: %d %c\n", j , fgetc(file_pointer)));
+  }
+  for (int j = i; i-j < 2; i ++ ){
+    DEBUG_PRINT(("place_holder: %d %c\n", j , fgetc(file_pointer)));
+  }
+  while ( i <= length ) {
+    place_holder = fgetc(file_pointer);
+    /* DEBUG_PRINT(("place_holder: %c", place_holder)); */
+    i = 1 + length;
+    /* if ( place_holder == EOF ){ */
+    /*   buffer[i] = '\0'; */
+    /*   break; */
+    /* } */
+    /* if ( isupper(place_holder) || isdigit(place_holder) ){ */
+    /*   buffer[i] = place_holder; */
+    /*   i++; */
+    /* } else { */
+    /*   /\* make sure the rest of the fomating is correct*\/ */
+    /*   while ( place_holder != EOF ) { */
+    /* 	if ( !isspace(place_holder) ) { */
+    /* 	  printf("Error: Invalid format\n"); */
+    /* 	  exit(3); */
+    /* 	} */
+    /* 	place_holder = fgetc(file_pointer); */
+    /*   } */
+    /*   buffer[i] = '\0'; */
+    /*   break; /\* formatting is correct so exit loop *\/ */
+    /* } */
+  }
+
+  /* DO SOME STUFF */
+  fclose(file_pointer);
 }
